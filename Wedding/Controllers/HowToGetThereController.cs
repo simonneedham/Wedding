6777fc5f5c0cac674e13eff9732512716f4e39f2
sn <git@needham.uk.net>
2012-08-26 22:16:36 +0100

    First drafts of three pages.
    HowtoGetThere, StayInTouch, WhereToStay

---
+++ Wedding/Controllers/HowToGetThereController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wedding.Controllers
+{
+    public class HowToGetThereController : Controller
+    {
+        //
+        // GET: /HowToGetThere/
+        [CompressFilter]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
