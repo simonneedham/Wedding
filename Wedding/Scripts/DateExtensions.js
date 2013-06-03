ffb1783af260b3de16f8a6b54f12aebdcffe3958
sn <git@needham.uk.net>
2013-06-03 23:36:52 +0100

    Allow Updated field to be edited.

---
+++ Wedding/Scripts/DateExtensions.js
@@ -0,0 +1,11 @@
+﻿Date.prototype.addMinutes = function (minutes) {
+    return new Date(this.getTime() + minutes * 60000);
+}
+
+Date.prototype.getUtcDateObject = function () {
+    return new Date(this.toUTCString());
+}
+
+Date.prototype.getLocalDateObject = function () {
+    return new Date(this.toLocaleString())
+}
